cf58ff237851b2a549d2f16789bdb599f8ab06bf
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-02-11 16:35:29 +0500

    Update formatting

--- get_keys.py
+++ get_keys.py
@@ -19,9 +19,11 @@ def main():
     parser = argparse.ArgumentParser(
             description='Generate private keys from password',
             epilog='Report bugs to: ')
-    parser.add_argument('-d', '--debug', action='store_true',
+    parser.add_argument(
+            '-d', '--debug', action='store_true',
             help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./config.yml',
+    parser.add_argument(
+            '-c', '--config', default='./config.yml',
             help='specify custom path for config file')
     parser.add_argument('account')
     parser.add_argument('password')
