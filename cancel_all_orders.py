e48d1ea2e559ba019506b7cd686a690776ba778b
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-03-03 15:02:42 +0500

    Rewrite cancel_all_orders.py using click

--- cancel_all_orders.py
+++ cancel_all_orders.py
@@ -2,29 +2,25 @@
 
 import sys
 import json
-import argparse
 import logging
 import yaml
+import click
 
 from bitshares import BitShares
 from bitshares.account import Account
 
 log = logging.getLogger(__name__)
 
-def main():
-
-    parser = argparse.ArgumentParser(
-            description='Cancel all orders on account',
-            epilog='Report bugs to: ')
-    parser.add_argument('-d', '--debug', action='store_true',
-            help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml',
-            help='specify custom path for config file')
-    parser.add_argument('account')
-    args = parser.parse_args()
-
+@click.command()
+@click.option('-d', '--debug', default=False, is_flag=True, help='enable debug output')
+@click.option('-c', '--config', type=click.File('r'), default='./config.yml',
+              help='specify custom path for config file')
+@click.option('--wallet-password', prompt=True, hide_input=True,
+              help='master password for bitshares wallet (a prompt will be used if not provided)')
+@click.argument('account')
+def main(debug, config, wallet_password, account):
     # create logger
-    if args.debug == True:
+    if debug == True:
         log.setLevel(logging.DEBUG)
     else:
         log.setLevel(logging.INFO)
@@ -34,13 +30,20 @@ def main():
     log.addHandler(handler)
 
     # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.load(ymlfile)
+    conf = yaml.load(config)
 
     bitshares = BitShares(node=conf['node_bts'])
-    a = Account(args.account, blockchain_instance=bitshares)
-    orders = [order['id'] for order in a.openorders if 'id' in order]
-    bitshares.cancel(orders, account=a)
+
+    # Wallet unlock
+    try:
+        bitshares.unlock(wallet_password)
+    except WrongMasterPasswordException:
+        log.critical('Wrong wallet password provided')
+        sys.exit(1)
+
+    account = Account(account, blockchain_instance=bitshares)
+    orders = [order['id'] for order in account.openorders if 'id' in order]
+    bitshares.cancel(orders, account=account)
 
 if __name__ == '__main__':
     main()
