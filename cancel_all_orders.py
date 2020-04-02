dbe4f08f1344b9a5c47800ce319356b390bdbec8
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-02 14:53:19 +0500

    Refactor to use new decorators

--- cancel_all_orders.py
+++ cancel_all_orders.py
@@ -1,66 +1,39 @@
 #!/usr/bin/env python
 
-import json
-import logging
 import sys
 
 import click
-import yaml
-from bitshares import BitShares
 from bitshares.account import Account
 from bitshares.market import Market
+from uptick.decorators import unlock
 
-log = logging.getLogger(__name__)
+from bitsharesscripts.decorators import chain, common_options
 
 
 @click.command()
-@click.option('-d', '--debug', default=False, is_flag=True, help='enable debug output')
-@click.option(
-    '-c', '--config', type=click.File('r'), default='./config.yml', help='specify custom path for config file'
-)
-@click.option(
-    '--wallet-password',
-    prompt=True,
-    hide_input=True,
-    help='master password for bitshares wallet (a prompt will be used if not provided)',
-)
+@common_options
+@chain
+@unlock
 @click.option('--buy-only', default=False, is_flag=True, help='cancel only buy orders')
 @click.option('--sell-only', default=False, is_flag=True, help='cancel only sell orders')
-@click.option('--market', help='cancel orders only on specified market only, format is BASE/QUOTE')
+@click.option('--market', help='cancel orders only on specified market only, format is QUOTE/BASE')
 @click.argument('account')
-def main(debug, config, wallet_password, buy_only, sell_only, market, account):
+@click.pass_context
+def main(ctx, buy_only, sell_only, market, account):
+    """ Cancel all orders of specified account
+
+        Optionally, you can select market like BTC/USD and choose buy/sell orders only
+    """
 
     if buy_only and sell_only:
-        log.critical('--buy-only and --sell-only are mutually exclusive')
+        ctx.log.critical('--buy-only and --sell-only are mutually exclusive')
         sys.exit(1)
 
-    # create logger
-    if debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    conf = yaml.safe_load(config)
-
-    bitshares = BitShares(node=conf['node_bts'])
-
-    # Wallet unlock
-    try:
-        bitshares.unlock(wallet_password)
-    except WrongMasterPasswordException:
-        log.critical('Wrong wallet password provided')
-        sys.exit(1)
-
-    account = Account(account, blockchain_instance=bitshares)
+    account = Account(account, blockchain_instance=ctx.bitshares)
     orders = [order for order in account.openorders if 'id' in order]
 
     if market:
-        market = Market(market, blockchain_instance=bitshares)
+        market = Market(market, blockchain_instance=ctx.bitshares)
         market_ids = [market['base']['symbol'], market['quote']['symbol']]
         orders = [
             order
@@ -74,8 +47,7 @@ def main(debug, config, wallet_password, buy_only, sell_only, market, account):
         orders = [order for order in orders if order['base']['symbol'] == market['quote']['symbol']]
 
     ids = [order['id'] for order in orders if 'id' in order]
-
-    bitshares.cancel(ids, account=account)
+    ctx.bitshares.cancel(ids, account=account)
 
 
 if __name__ == '__main__':
