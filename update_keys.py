cc7eb2f97a620cf7ea8072e66163b80d253d898e
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-03 13:12:15 +0500

    Refactor to use new decorators

--- update_keys.py
+++ update_keys.py
@@ -1,70 +1,37 @@
 #!/usr/bin/env python
 
-import logging
 import sys
+from pprint import pformat
 
 import click
-import yaml
-from bitshares import BitShares
 from bitshares.account import Account
 from bitshares.exceptions import MissingKeyError
 from bitsharesbase import operations
 from bitsharesbase.account import PasswordKey
-from graphenestorage.exceptions import WrongMasterPasswordException
+from uptick.decorators import unlock
 
+from bitsharesscripts.decorators import chain, common_options
 from bitsharesscripts.functions import generate_password
-
-log = logging.getLogger(__name__)
 
 key_types = ['active', 'owner', 'memo']
 
 
 @click.command()
-@click.option('-d', '--debug', default=False, is_flag=True, help='enable debug output')
-@click.option(
-    '-c', '--config', type=click.File('r'), default='./config.yml', help='specify custom path for config file'
-)
-@click.option(
-    '--wallet-password',
-    prompt=True,
-    hide_input=True,
-    help='master password for bitshares wallet (a prompt will be used if not provided)',
-)
+@common_options
+@chain
+@unlock
 @click.option('-p', '--password', help='manually specify a password (if not, a random will be generated)')
 @click.option('--broadcast', default=False, is_flag=True, help='broadcast transaction')
 @click.argument('account_name')
-def main(debug, config, wallet_password, password, broadcast, account_name):
+@click.pass_context
+def main(ctx, password, broadcast, account_name):
     """ Use this script to change account keys. By default, a random will be
         generated. By default, transaction will not be broadcasted (dry-run mode).
     """
-    # create logger
-    if debug is True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    conf = yaml.safe_load(config)
-
-    b = not broadcast
-    bitshares = BitShares(node=conf['node_bts'], nobroadcast=b)
-    account = Account(account_name, bitshares_instance=bitshares)
-
-    # Wallet unlock
-    try:
-        bitshares.unlock(wallet_password)
-    except WrongMasterPasswordException:
-        log.critical('Wrong wallet password provided')
-        sys.exit(1)
+    account = Account(account_name, bitshares_instance=ctx.bitshares)
 
     # random password
-    if password:
-        password = password
-    else:
+    if not password:
         password = generate_password()
 
     print('password: {}\n'.format(password))
@@ -81,7 +48,7 @@ def main(debug, config, wallet_password, password, broadcast, account_name):
         pubkey = k.get_public_key()
 
         # pubkey with correct prefix
-        key[key_type] = format(pubkey, bitshares.prefix)
+        key[key_type] = format(pubkey, ctx.bitshares.prefix)
         print('{} public: {}\n'.format(key_type, key[key_type]))
 
     # prepare for json format
@@ -102,16 +69,20 @@ def main(debug, config, wallet_password, password, broadcast, account_name):
         },
         'fee': {'amount': 0, 'asset_id': '1.3.0'},
         'extensions': {},
-        'prefix': bitshares.prefix,
+        'prefix': ctx.bitshares.prefix,
     }
 
-    # pprint(s)
+    ctx.log.debug(pformat(s))
     op = operations.Account_update(**s)
 
+    if not broadcast:
+        ctx.log.info('Not broadcasting!')
+        sys.exit(0)
+
     try:
-        bitshares.finalizeOp(op, account_name, 'owner')
+        ctx.bitshares.finalizeOp(op, account_name, 'owner')
     except MissingKeyError:
-        log.critical('No key for {} in storage, use `uptick addkey` to add'.format(account_name))
+        ctx.log.critical('No key for {} in storage, use `uptick addkey` to add'.format(account_name))
         sys.exit(1)
 
 
