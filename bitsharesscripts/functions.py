c85d8c78e963b57658bae9a940987d60cc696265
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-03 21:42:02 +0500

    Fix linting errors

--- bitsharesscripts/functions.py
+++ bitsharesscripts/functions.py
@@ -6,29 +6,27 @@ from bitsharesbase.account import PasswordKey
 
 
 def generate_password(size: int = 53, chars: str = string.ascii_letters + string.digits) -> str:
-    """ Generate random word with letters and digits
-    """
+    """Generate random word with letters and digits."""
     return ''.join(secrets.choice(chars) for x in range(size))
 
 
 def get_keys_from_password(
     account_name: str, password: str, blockchain_instance: Any, key_types: Optional[List[str]] = None,
 ) -> Dict[str, str]:
-    """ Generates public/private keys from a password
-    """
+    """Generates public/private keys from a password."""
     if not key_types:
         key_types = ['active', 'owner', 'memo']
 
-    keys = dict()
+    keys = {}
     for key_type in key_types:
         # PasswordKey object
-        k = PasswordKey(account_name, password, role=key_type)
+        passkey = PasswordKey(account_name, password, role=key_type)
 
-        privkey = k.get_private_key()
+        privkey = passkey.get_private_key()
         print('{} private: {}'.format(key_type, str(privkey)))  # we need explicit str() conversion!
 
         # pubkey with default prefix GPH
-        pubkey = k.get_public_key()
+        pubkey = passkey.get_public_key()
 
         # pubkey with correct prefix
         keys[key_type] = format(pubkey, blockchain_instance.prefix)
