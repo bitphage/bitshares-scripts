3e6b1063d4a8d877acf98896d639deceb3cc6f65
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-04 15:34:31 +0500

    Refactor get_account.py

--- get_account.py
+++ get_account.py
@@ -1,42 +1,21 @@
 #!/usr/bin/env python
 
-import argparse
-import json
-import logging
-import sys
 from pprint import pprint
 
-import yaml
-from bitshares import BitShares
+import click
 from bitshares.account import Account
 
-log = logging.getLogger(__name__)
+from bitsharesscripts.decorators import chain, common_options
 
 
-def main():
-
-    parser = argparse.ArgumentParser(description='Generate private keys from password', epilog='Report bugs to: ')
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./config.yml', help='specify custom path for config file')
-    parser.add_argument('account')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    bitshares = BitShares(node=conf['node_bts'], no_broadcast=True)
-    account = Account(args.account, bitshares_instance=bitshares)
+@click.command()
+@common_options
+@chain
+@click.argument('account')
+@click.pass_context
+def main(ctx, account):
+    """Show account object."""
+    account = Account(account, bitshares_instance=ctx.bitshares)
     pprint(dict(account))
 
 
