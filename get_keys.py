e71d07783dbf190fe753c90aabfe7835414826d7
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-05 00:11:17 +0500

    Refactor get_keys.py

--- get_keys.py
+++ get_keys.py
@@ -1,46 +1,20 @@
 #!/usr/bin/env python
+import click
 
-import argparse
-import json
-import logging
-import sys
-from pprint import pprint
-
-import yaml
-from bitsharesbase.account import PasswordKey, PublicKey
-
-log = logging.getLogger(__name__)
-
-roles = ['active', 'owner', 'memo']
+from bitsharesscripts.decorators import chain, common_options
+from bitsharesscripts.functions import get_keys_from_password
 
 
-def main():
+@click.command()
+@common_options
+@chain
+@click.argument('account_name')
+@click.argument('password')
+@click.pass_context
+def main(ctx, account_name, password):
+    """Generate private keys from password."""
 
-    parser = argparse.ArgumentParser(description='Generate private keys from password', epilog='Report bugs to: ')
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./config.yml', help='specify custom path for config file')
-    parser.add_argument('account')
-    parser.add_argument('password')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    for role in roles:
-        key = PasswordKey(args.account, args.password, role=role)
-        privkey = key.get_private_key()
-        print('role: {}, key: {}'.format(role, str(privkey)))
+    get_keys_from_password(account_name, password, ctx.bitshares)
 
 
 if __name__ == '__main__':
