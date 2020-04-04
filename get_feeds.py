bf9cd491aef16780074a7d1269b7221c0ff0e2e5
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-05 00:14:42 +0500

    Refactor get_feeds.py

--- get_feeds.py
+++ get_feeds.py
@@ -1,43 +1,19 @@
 #!/usr/bin/env python
 
-import argparse
-import json
-import logging
-import sys
-from pprint import pprint
-
-import yaml
-from bitshares import BitShares
-from bitshares.account import Account
+import click
 from bitshares.asset import Asset
 
-log = logging.getLogger(__name__)
+from bitsharesscripts.decorators import chain, common_options
 
 
-def main():
-
-    parser = argparse.ArgumentParser(description='Print current feeds for asset', epilog='Report bugs to: ')
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./config.yml', help='specify custom path for config file')
-    parser.add_argument('asset')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    bitshares = BitShares(node=conf['node_bts'], no_broadcast=True)
-    asset = Asset(args.asset, bitshares_instance=bitshares)
+@click.command()
+@common_options
+@chain
+@click.argument('asset')
+@click.pass_context
+def main(ctx, asset):
+    """Print current feeds for an asset."""
+    asset = Asset(asset, bitshares_instance=ctx.bitshares)
     feeds = asset.feeds
 
     for feed in feeds:
