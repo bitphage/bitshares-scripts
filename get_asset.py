44b529ba8d5e314c509a5dfb3cf857a2e961e6fe
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-05-18 14:20:00 +0500

    Add simple output for get_asset.py

--- get_asset.py
+++ get_asset.py
@@ -6,18 +6,34 @@ import click
 from bitshares.asset import Asset
 
 from bitsharesscripts.decorators import chain, common_options
+from bitsharesscripts.functions import raw_to_decimal
 
 
 @click.command()
 @common_options
 @chain
+@click.option('--raw', is_flag=True, default=False, help='show raw asset data')
 @click.argument('asset')
 @click.pass_context
-def main(ctx, asset):
+def main(ctx, raw, asset):
     """Get asset info."""
 
     asset = Asset(asset, full=True, bitshares_instance=ctx.bitshares)
-    pprint(dict(asset))
+
+    if raw:
+        pprint(dict(asset))
+        return
+
+    print('id: {}'.format(asset['id']))
+    supply = raw_to_decimal(asset['dynamic_asset_data']['current_supply'], asset['precision'])
+    print('supply: {} {}'.format(supply, asset['symbol']))
+
+    if asset.is_bitasset:
+        feed = asset.feed
+        print('MCR: {}'.format(feed['maintenance_collateral_ratio']))
+        print('MSSR: {}'.format(feed['maximum_short_squeeze_ratio']))
+        print('Settlement: {}'.format(feed['settlement_price']))
+        print('CER: {}'.format(feed['core_exchange_rate']))
 
 
 if __name__ == '__main__':
